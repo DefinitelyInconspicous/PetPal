342ca28e9102c8af2a571406f79ff0023c9d03f0
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-07-20 10:43:03 +0800

    Started on Home Page

--- PetPal/Home Page.swift
+++ PetPal/Home Page.swift
@@ -7,12 +7,52 @@
 
 import SwiftUI
 
+
+
 struct Home_Page: View {
+    var textColour = Color(red: 34, green: 53, blue: 64)
+    @State var username = "John"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack {
+                Text("Pet")
+                    .font(Font.system(size:60, design: .rounded))
+                    .fontWeight(.heavy)
+                    .fontDesign(.rounded)
+                    .padding()
+                    .foregroundColor(textColour)
+                Spacer()
+                
+                // MARK: - Add Daily Sticker
+                
+                Text("Pal")
+                    .font(Font.system(size:60, design: .rounded))
+                    .fontWeight(.heavy)
+                    .fontDesign(.rounded)
+                    .padding()
+            }
+            Rectangle()
+                .frame(width: 6000 , height: 4)
+            Text("Welcome Back \(username)")
+            
+            Spacer()
+        }
     }
 }
 
 #Preview {
     Home_Page()
 }
+
+
+
+
+
+
+
+
+
+
+
+
