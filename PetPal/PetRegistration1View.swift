b275556dd5073d9a8e2b47ce0b11c6b034c9faca
Calvin Abad <calvin.abad@gmail.com>
2024-07-20 11:55:25 +0800

    Did more work on the first part of Pet Registration. Weight, DOB, gender and sterilisation status have been added

--- PetPal/PetRegistration1View.swift
+++ PetPal/PetRegistration1View.swift
@@ -11,6 +11,12 @@ struct PetRegistration1View: View {
     @State private var setPetName = ""
     @State private var setPetType = ""
     @State private var setBreed = ""
+    @State private var setWeight = 0
+    @State private var setAge = 0
+    @State private var setDiet = ""
+    @State private var setGender = false
+    @State private var birthDate = Date.now
+    @State private var setSterile = false
     var body: some View {
         NavigationView{
             VStack{
@@ -29,6 +35,30 @@ struct PetRegistration1View: View {
                 } label: {
                   Text("Pet Breed")
                 }
+                LabeledContent{
+                    Stepper("\(setWeight) kg", value: $setWeight, step: 1)
+                }label:{
+                    Text("Weight")
+                }
+                LabeledContent{
+                    Stepper("\(setAge) years", value: $setAge, step: 1)
+                }label:{
+                    Text("Age")
+                }
+                LabeledContent {
+                  TextField("Diet", text: $setDiet)
+                } label: {
+                  Text("Pet Diet")
+                }
+                LabeledContent{
+                    Toggle(setGender ? "Male" : "Female", isOn: $setGender)
+                }label:{
+                    Text("Gender")
+                }
+                DatePicker(selection: $birthDate, in: ...Date.now, displayedComponents: .date) {
+                        Text("Select date of birth")
+                            }
+                Toggle("Sterilised", isOn: $setSterile)
             }
         }
         .navigationTitle("Register your pet!")
