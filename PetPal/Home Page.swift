944653a1cd03d957b3f41a0c8d799224cdca7baa
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-07-20 12:19:58 +0800

    Added weight graph logic WARNING UNSOLVED BUG!

--- PetPal/Home Page.swift
+++ PetPal/Home Page.swift
@@ -6,14 +6,26 @@
 //
 
 import SwiftUI
+import Charts
 
-
-
+struct weightCell {
+    var weight: Int
+    var date: Int
+}
 struct Home_Page: View {
     var textColour = Color(red: 34 / 255, green: 53 / 255, blue: 64 / 255)
+    var rectColour = Color(red: 242/255, green: 241/255, blue: 216/255)
     @State var username = "John"
+     var weightGraph = [20,23,18,28,32,14,29,45,49,34,46,29,19,20,28,38,41,49,46,54,61]
+    @State var timeGraph = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+    @State var weightData: [weightCell]
+    @State var weightCount = 0
     
     var body: some View {
+        ForEach(weightGraph, id: \.self) { weightVal in
+            weightData.append(weightCell(weight: weightVal, date: timeGraph[weightGraph.firstIndex(of: weightVal) ?? 0]))
+            weightCount += 1
+        }
         VStack{
             HStack {
                 Text("Pet")
@@ -39,15 +51,32 @@ struct Home_Page: View {
             Text("Welcome Back \(username)!")
                 .font(.title2)
                 .fontWeight(.medium)
-            Rectangle()
-                .frame(width: 350, height: 170)
+            ZStack {
+                Rectangle()
+                    .frame(width: 350, height: 170)
+                    .cornerRadius(20)
+                    .foregroundColor(rectColour)
+                VStack {
+                 Text("Your Pet's Weight over the Months")
+                        .font(.headline)
+                    Chart {
+                        ForEach(weightGraph, id: \.self ) { weightVal in
+                           
+                            LineMark(x: .value("Date", timeGraph[weightCount]), y: weightData[weightCount])
+                            
+                            
+                        }
+                    }
+                }
+                
+            }
             Spacer()
         }
     }
 }
 
 #Preview {
-    Home_Page()
+    Home_Page(weightData: [weightCell(weight: 0, date: 0)])
 }
 
 
