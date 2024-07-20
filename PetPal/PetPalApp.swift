9f072b41b012b3bba30ef2598ec400c97618d7eb
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-07-20 09:52:02 +0800

    Initial Commit

---
+++ PetPal/PetPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetPalApp.swift
+//  PetPal
+//
+//  Created by Avyan Mehra on 20/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PetPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
