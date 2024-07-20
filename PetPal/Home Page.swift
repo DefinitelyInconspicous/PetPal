c05b57187a11dc2d2a35271c25d28cedea5c7485
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-07-20 10:27:59 +0800

    Started on Home Page

---
+++ PetPal/Home Page.swift
@@ -0,0 +1,18 @@
+//
+//  Home Page.swift
+//  PetPal
+//
+//  Created by Avyan Mehra on 20/7/24.
+//
+
+import SwiftUI
+
+struct Home_Page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home_Page()
+}
