4c37c984ced008587e7c654ea7635299d14cae36
ishika0709 <ishtholia19@gmail.com>
2024-07-27 12:08:08 +0800

    started work on the settings view

---
+++ PetPal/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  PetPal
+//
+//  Created by Ishika Tholia on 27/7/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
