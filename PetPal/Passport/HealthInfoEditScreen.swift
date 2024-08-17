2d3885b50c5a248d01e72ed95b89a448a357e93c
Calvin Abad <calvin.abad@gmail.com>
2024-08-17 10:05:32 +0800

    Started passport 2 edit, added notifications

---
+++ PetPal/Passport/HealthInfoEditScreen.swift
@@ -0,0 +1,19 @@
+//
+//  HealthInfoEditScreen.swift
+//  PetPal
+//
+//  Created by Calvin Abad on 17/8/24.
+//
+
+import SwiftUI
+import Forever
+
+struct HealthInfoEditScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HealthInfoEditScreen()
+}
--- PetPal/SettingsView.swift
+++ PetPal/SettingsView.swift
@@ -11,6 +11,8 @@ import Forever
 struct SettingsView: View {
 
     @Forever("notifications") var notifications = false
+    @Forever("cameraAccess") var cameraAccess = false
+    @Forever("photoAccess") var photoAccess = false
     var body: some View {
         NavigationView{
             List{
@@ -18,24 +20,34 @@ struct SettingsView: View {
                     Toggle("Toggle Reminders", isOn: $notifications)
                 }
                 Section{
-                    Toggle("Allow Access To Camera", isOn: $notifications)
-                    Toggle("Allow Access To Photos", isOn: $notifications)
+                    Toggle("Allow Access To Camera", isOn: $cameraAccess)
+                    Toggle("Allow Access To Photos", isOn: $photoAccess)
                 }
             }.onChange(of: notifications) { _ in
+                if notifications == true{
+                    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                        if success {
+                            print("All set!")
+                            let content = UNMutableNotificationContent()
+                            content.title = "Spend time with your pet!"
+                            content.subtitle = "It seems lonely!"
+                            content.sound = UNNotificationSound.default
+                            
+                            // show this notification five seconds from now
+                            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1800, repeats: true)
+                            
+                            // choose a random identifier
+                            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                            
+                            // add our notification request
+                            UNUserNotificationCenter.current().add(request)
+                        } else if let error {
+                            print(error.localizedDescription)
+                        }
+                    }
+                }
                 
-                    let content = UNMutableNotificationContent()
-                    content.title = "Feed the cat"
-                    content.subtitle = "It looks hungry"
-                    content.sound = UNNotificationSound.default
-
-                    // show this notification five seconds from now
-                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 7200, repeats: true)
-
-                    // choose a random identifier
-                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
-                    // add our notification request
-                    UNUserNotificationCenter.current().add(request)
+                    
                 }
         }
     }
