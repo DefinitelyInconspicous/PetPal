4296a9cca7075755d6a7487f06da80ee65d8fe13
Calvin Abad <calvin.abad@gmail.com>
2024-08-17 10:42:53 +0800

    Passport screen 2 work: Added an edit function

--- PetPal/Passport/PassportScreen2.swift
+++ PetPal/Passport/PassportScreen2.swift
@@ -10,6 +10,7 @@ import Forever
 struct PassportScreen2: View {
     @Forever("medInfo") var medInfo = PetMedicalConditions(allergies: ["Peanuts"], healthIssues: ["Obesity"], medications: ["Panadol"])
     @State var editToggle = false
+    @State var editCategory = 0
     var body: some View {
         NavigationView{
             VStack{
@@ -24,24 +25,51 @@ struct PassportScreen2: View {
                             Text("Allergies")
                             Spacer()
                             Button {
+                                editCategory = 0
+                                editToggle = true
                                 
                             } label: {
                                 Image(systemName:"plus")
                             }
                         }
                     }
-                    Section(header:Text("Health Issues")){
+                    Section(){
                         ForEach(0..<medInfo.healthIssues.count){healthIssue in
                             Text(medInfo.healthIssues[healthIssue])
                         }
+                    } header: {
+                        HStack {
+                            Text("Health Issues")
+                            Spacer()
+                            Button {
+                                editCategory = 1
+                                editToggle = true
+                            } label: {
+                                Image(systemName:"plus")
+                            }
+                        }
                     }
-                    Section(header:Text("Medications")){
+                    Section(){
                         ForEach(0..<medInfo.medications.count){medication in
                             Text(medInfo.medications[medication])
+                        }
+                    } header: {
+                        HStack {
+                            Text("Medications")
+                            Spacer()
+                            Button {
+                                editCategory = 2
+                                editToggle = true
+                            } label: {
+                                Image(systemName:"plus")
+                            }
                         }
                     }
                 }
             }
+        }
+        .sheet(isPresented: $editToggle){
+            HealthInfoEditScreen(infoSwitch: editCategory)
         }
     }
 }
