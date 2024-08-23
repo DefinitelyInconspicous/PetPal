9c15fae27dccbe27b6aa33c229ee9e28301ebd8b
Calvin Abad <calvin.abad@gmail.com>
2024-08-24 00:43:35 +0800

    Added reset page for debug purposes

---
+++ PetPal/SlateBlankerView.swift
@@ -0,0 +1,18 @@
+//
+//  SlateBlankerView.swift
+//  PetPal
+//
+//  Created by Calvin Abad on 24/8/24.
+//
+
+import SwiftUI
+
+struct SlateBlankerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SlateBlankerView()
+}
